4412e95b4f267c34b9b8e4fb2b36e76502d7f170
Prasoon1308 <prasoon1308@gmail.com>
2024-02-01 18:14:07 +0530

    Token contracts updated

--- contracts/TokenA.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
-
-import "./TokenBase.sol";
-
-contract TokenA is TokenBase {
-    constructor() TokenBase("Token A", "TA") {}
-}
--- contracts/TokenB.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
-
-import "./TokenBase.sol";
-
-contract TokenB is TokenBase {
-    constructor() TokenBase("Token B", "TB") {}
-}
